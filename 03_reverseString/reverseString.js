5dc0720c7483678390c19bd561b857b1d1e94413
Gurkaran <gurkaransinghk05@gmail.com>
2023-04-27 06:09:03 -0400

    convert to array with split() then reverse & join

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
-};
+const reverseString = function(string) {
+    let stringArray = string.split("") //split basically converts strings to array
+    stringArray = stringArray.reverse().join('')//reverse() reverses the string characters and join converts array to string again
+    return (stringArray)
+  
+  };
+  
+  reverseString('')
 
 // Do not edit below this line
 module.exports = reverseString;
